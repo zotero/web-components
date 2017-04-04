dc27c58a60acf32d845b9f658d06ea9ef51099c7
Faolan C-P <fcheslack@gmail.com>
2017-04-04 17:03:41 -0400

    querystring utils and readCookie

--- src/js/Utils.js
+++ src/js/Utils.js
@@ -9,4 +9,116 @@ let slugify = function(name){
 	return slug;
 };
 
-export {slugify};
+/**
+ * Given a query string, parse keys/values into an object
+ **/
+let parseQuery = function(query) {
+	var params = {};
+	var match;
+	var pl = /\+/g;  // Regex for replacing addition symbol with a space
+	var search = /([^&=]+)=?([^&]*)/g;
+	var decode = function (s) {
+		return decodeURIComponent(s.replace(pl, ' '));
+	};
+
+	while((match = search.exec(query)) !== null) {
+		let key = decode(match[1]);
+		let val = decode(match[2]);
+		if(params[key]){
+			params[key] = [].concat(params[key], val);
+		} else {
+			params[key] = val;
+		}
+	}
+	return params;
+};
+
+let buildQuery = function(params = {}) {
+	let q = '?';
+	let ar = [];
+	for(let p in params) {
+		ar.push(`${encodeURIComponent(p)}=${encodeURIComponent(params[p])}`);
+		//q += `&${encodeURIComponent(p)}=${encodeURIComponent(params[p])}`;
+	}
+	q += ar.join('&');
+	return q;
+};
+
+//extract the section of a url between ? and #
+let querystring = function(href) {
+	if(href.indexOf('?') == -1) {
+		return '';
+	}
+	var hashindex = (href.indexOf('#') != -1) ? href.indexOf('#') : undefined;
+	var q = href.substring(href.indexOf('?') + 1, hashindex);
+	return q;
+};
+
+//parse the string from a search box to pull out variables for things other than a basic phrase search
+let parseSearchString = function(search) {
+	let params = {};
+	let matches = search.match(/doi\:(\S+)/);
+	if(matches){
+		params.doi = matches[1];
+	}
+
+	matches = search.match(/isbn\:(\S+)/);
+	if(matches){
+		params.isbn = matches[1];
+	}
+
+	matches = search.match(/pmid\:(\S+)/);
+	if(matches){
+		params.pmid = matches[1];
+	}
+
+	matches = search.match(/pmcid\:(\S+)/);
+	if(matches){
+		params.pmcid = matches[1];
+	}
+
+	matches = search.match(/url\:(\S+)/);
+	if(matches){
+		params.url = matches[1];
+	}
+
+	matches = search.match(/urlbase\:(\S+)/);
+	if(matches){
+		params.urlbase = matches[1];
+	}
+
+	matches = search.match(/title\:(\S+)/);
+	if(matches){
+		params.title = matches[1];
+	}
+
+	matches = search.match(/creators\:(\S+)/);
+	if(matches){
+		params.creators = matches[1];
+	}
+
+	matches = search.match(/itemType\:(\S+)/);
+	if(matches){
+		params.itemType = matches[1];
+	}
+
+	params.q = search.replace(/\S+:\S+/g, '').trim();
+	if(params.q == ''){
+		delete params.q;
+	}
+
+	return params;
+};
+
+let readCookie = function(name) {
+	var nameEQ = name + '=';
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+};
+
+export {slugify, parseQuery, buildQuery, querystring, parseSearchString, readCookie};

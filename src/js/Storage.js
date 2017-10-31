15585af70c130dbdbfe9759bb293f15fee9a4dbd
Faolan C-P <fcheslack@gmail.com>
2017-10-31 17:00:27 -0400

    fix unlimited institution message

--- src/js/Storage.js
+++ src/js/Storage.js
@@ -36,7 +36,7 @@ let plans = [
 		description: 'Unlimited',
 		price: '$120',
 		discountedPrice: '$96'
-	}	
+	}
 ];
 
 var calculateRemainingValue = function(expiration=Date.now(), storageLevel=2) {
@@ -129,8 +129,12 @@ StoragePlanRow.propTypes = {
 class InstitutionProvides extends Component {
 	render(){
 		let institution = this.props.institution;
+		let quotaDescription = `${institution.storageQuota} MB of storage`;
+		if(institution.storageQuota == 1000000){
+			quotaDescription = 'unlimited storage';
+		}
 		return (
-			<p>{institution.name} provides {institution.storageQuota} MB for {institution.email}</p>
+			<p>{institution.name} provides {quotaDescription} for {institution.email}</p>
 		);
 	}
 }

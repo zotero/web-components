76f1ba7f36ddf44de42bce59f61de37c61e72185
Faolan C-P <fcheslack@gmail.com>
2017-11-13 16:56:38 -0500

    add fonts-win to build

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,7 @@ function getJS(dev) {
 }
 
 function getSass(dev) {
-	return gulp.src(['src/scss/web-components.scss', 'src/scss/fonts-mac.scss'])
+	return gulp.src(['src/scss/web-components.scss', 'src/scss/fonts-mac.scss', 'src/scss/fonts-win.scss'])
 		.pipe(plumber({errorHandler: onError}))
 		.pipe(gulpif(dev, sourcemaps.init({loadMaps: true})))
 		.pipe(sass())

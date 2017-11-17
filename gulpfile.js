8e55a82b702702ebde9deb0d506370e93a853f95
Faolan C-P <fcheslack@gmail.com>
2017-11-17 11:29:42 -0500

    add flexibility polyfill

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ const runSequence = require('run-sequence');
 const del = require('del');
 const sass = require('gulp-sass');
 const babelify = require('babelify');
+const postcss    = require('gulp-postcss');
 
 const babelifyOpts = {
 	extensions: ['.js', '.jsx'],
@@ -89,6 +90,7 @@ function getSass(dev) {
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions', 'IE 10']
 		}))
+		.pipe(postcss([ require('postcss-flexibility') ]))
 		.pipe(gulpif(!dev, gulp.dest('./build')))
 		.pipe(gulpif(!dev, cssminify()))
 		.pipe(gulpif(!dev, rename({ extname: '.min.css' })))

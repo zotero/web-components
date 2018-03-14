b401da4ba6be7f7ae559f420d34a5accdf21b198
Faolan C-P <fcheslack@gmail.com>
2018-03-14 14:10:39 -0400

    don't use libZotero groups helper for profile page

--- src/js/profile/abstract/groups-base.jsx
+++ src/js/profile/abstract/groups-base.jsx
@@ -3,14 +3,29 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import {ajax} from '../../ajax.js';
+import {apiRequestString} from '../../ApiRouter.js';
+import {buildUrl} from '../../wwwroutes.js';
 
 var cache = {};
 
 export default class GroupsBase extends React.Component {
 	fetchUserGroups(userid) {
 		if(!cache[userid]) {
-			let groups = new Zotero.Groups();
-			cache[userid] = groups.fetchUserGroups(this.props.userid);
+			let url = apiRequestString({
+				'target':'userGroups',
+				'libraryType':'user',
+				'libraryID': this.props.userid,
+				'order':'title',
+				'limit':25,
+				'start':0
+			});
+			cache[userid] = ajax({url: url, credentials:'omit'}).then((resp)=>{
+				let totalResults = parseInt(resp.headers.get('Total-Results'));
+				resp.json().then((data) => {
+					return data;
+				});
+			});
 		}
 		
 		return cache[userid];

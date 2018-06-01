98386ecafc80983692646d591e0cfe0b394df803
Flachware <johannes.krtek@flachware.com>
2018-06-01 15:35:08 +0200

    Add generic single column page.

---
+++ demo/generic-single-col.php
@@ -0,0 +1,11 @@
+<? include('./layoutHeader.php');?>
+<main>
+	<section class="section section-md">
+		<div class="container-fluid container-fluid-col-10">
+			<h1>Heading 1</h1>
+			<p class="lead">Lead text</p>
+			<p>Paragraph</p>
+		</div>
+	</section>
+</main>
+<? include('./layoutFooter.php')?>

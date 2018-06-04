33ee5e538fdaf71f2f7d20543ba29f060b5a5024
Faolan C-P <fcheslack@gmail.com>
2018-06-04 16:46:45 -0400

    update group creation

--- src/js/CreateGroup.js
+++ src/js/CreateGroup.js
@@ -6,6 +6,7 @@
 import {ajax} from './ajax.js';
 import {slugify, readCookie} from './Utils.js';
 import {buildUrl} from './wwwroutes.js';
+import cn from 'classnames';
 
 let React = require('react');
 
@@ -15,52 +16,52 @@ class CreateGroup extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			name: '',
-			type: 'PublicOpen',
+			groupName: '',
+			selectedType: 'PublicOpen',
 			checkingName:false,
-			nameValid:null,
+			groupNameValid:null,
 			preview:''
 		};
-		this.changeType = this.changeType.bind(this);
-		this.changeName = this.changeName.bind(this);
 	}
-	changeType(evt) {
-		this.setState({type:evt.target.value});
-		this.changeName();
+	changeType = (evt) => {
+		let newType = evt.currentTarget.getAttribute('data-grouptype');
+		this.setState({selectedType:newType, groupNameValid:null}, this.checkName);
 	}
-	changeName(evt) {
-		if(evt){
-			this.setState({name:evt.target.value, nameValid:null});
-		}
-
-		clearTimeout(this.state.timer);
+	changeName = (evt) => {
+		this.setState({groupName:evt.target.value, groupNameValid:null}, this.checkName);
+	}
+	checkName = () => {
+		let {groupName, selectedType, timer} = this.state;
+		clearTimeout(timer);
 		let timeout = setTimeout(() => {
-			let groupType = this.state.type;
-
-			if(groupType != 'Private'){
+			if(selectedType != 'Private'){
 				// Poll the server with the input value
-				ajax({url:buildUrl('checkGroupName', {name:this.state.name})}).then((resp)=>{
+				ajax({url:buildUrl('checkGroupName', {name:groupName})}).then((resp)=>{
 					resp.json().then((data) => {
-						this.setState({nameValid:data.valid});
+						this.setState({groupNameValid:data.valid});
 					});
 				});
+			} else {
+				this.setState({groupNameValid:true});
 			}
 		}, 300);
 		this.setState({timer:timeout});
 	}
 	render() {
 		let slugPreview = '';
-		if(this.state.type == 'Private') {
+		const {selectedType, groupName, groupNameValid} = this.state;
+
+		if(selectedType == 'Private') {
 			slugPreview = `Group URL: ${config.baseZoteroWebsiteUrl}/groups/<number>`;
 		} else {
-			let slug = slugify(this.state.name);
+			let slug = slugify(groupName);
 			slugPreview = `Group URL: ${config.baseZoteroWebsiteUrl}/groups/${slug}`;
 		}
 		let slugStyle = {};
-		if(this.state.type != 'Private'){
-			if(this.state.nameValid === true){
+		if(selectedType != 'Private'){
+			if(groupNameValid === true){
 				slugStyle.color = 'green';
-			} else if(this.state.nameValid === false){
+			} else if(groupNameValid === false){
 				slugStyle.color = 'red';
 			}
 		}
@@ -70,56 +71,87 @@ class CreateGroup extends React.Component{
 			sessionKey = '';
 		}
 		return (
-			<div>
+			<div id='create-group'>
 				<h1>Create a New Group</h1>
 				<form encType="application/x-www-form-urlencoded" acceptCharset="utf-8" method="post" className="zform" action="">
 					<div className="row">
 						<div className="col-md-6">
 							<div className="form-group">
-								<label htmlFor="name" className="required">Group Name</label>
-								<input className="form-control" type="text" name="name" id="name" size="60" value={this.state.name} onChange={this.changeName} />
+								<input
+									className={cn('form-control', 'form-control-lg', {'is-invalid':(groupNameValid === false), 'is-valid':(groupNameValid === true)})}
+									type='text'
+									name='groupName'
+									id='groupName'
+									placeholder='Group Name'
+									onChange={this.changeName}
+									onBlur={this.checkName}
+									value={groupName}>
+								</input>
+
 								<label id='slugpreview' style={slugStyle}>{slugPreview}</label>
 							</div>
 						</div>
 					</div>
 					<div className="row">
-						<div className="card">
-							<div id="public-open" className="card-body">
-								<h4 className='card-title'>Public, Open Membership</h4>
-								<p className='muted small'>Anyone can view your group online and join the group instantly.</p>
-								<label htmlFor="group_type-PublicOpen">
-									<input type="radio" name="group_type" id="group_type-PublicOpen" onChange={this.changeType} value="PublicOpen" checked={this.state.type=='PublicOpen'} />
-									Choose a Public, Open Membership
-								</label>
+						<div className='card-deck'>
+							<div
+								className={cn("card", {selected:(selectedType == 'PublicOpen')})}
+								onClick={this.changeType}
+								data-grouptype='PublicOpen'
+								>
+								<div id="public-open" className="card-body">
+									<h4 className='card-title'>Public, Open Membership</h4>
+									<p className='muted small'>Anyone can view your group online and join the group instantly.</p>
+									<div className='form-check'>
+										<input className="form-check-input" type="radio" id="publicopenradio" value='PublicOpen' checked={selectedType == 'PublicOpen'} />
+										<label className="form-check-label" htmlFor="publicopenradio">
+											Select public, open membership
+										</label>
+									</div>
+								</div>
 							</div>
-						</div>
-						<div className="card">
-							<div id="public-closed" className="card-body">
-								<h4 className='card-title'>Public, Closed Membership</h4>
-								<p className='muted small'>Anyone can view your group online, but members must apply or be invited.</p>
-								<label htmlFor="group_type-PublicClosed">
-									<input type="radio" name="group_type" id="group_type-PublicClosed" onChange={this.changeType} value="PublicClosed" checked={this.state.type=='PublicClosed'} />
-									Choose Public, Closed Membership
-								</label>
+							<div
+								className={cn("card", {selected:(selectedType == 'PublicClosed')})}
+								onClick={this.changeType}
+								data-grouptype='PublicClosed'
+								>
+								<div id="public-closed" className="card-body">
+									<h4 className='card-title'>Public, Closed Membership</h4>
+									<p className='muted small'>Anyone can view your group online, but members must apply or be invited.</p>
+									<div className='form-check'>
+										<input className="form-check-input" type="radio" id="publicclosedradio" value='PublicClosed' checked={selectedType == 'PublicClosed'} />
+										<label className="form-check-label" htmlFor="publicclosedradio">
+											Select public, closed membership
+										</label>
+									</div>
+								</div>
 							</div>
-						</div>
-						<div className="card">
-							<div id="private" className="card-body">
-								<h4 className='card-title'>Private Membership</h4>
-								<p className='muted small'>Only members can view your group online and must be invited to join.</p>
-								<label htmlFor="group_type-Private">
-									<input type="radio" name="group_type" id="group_type-Private" onChange={this.changeType} value="Private" checked={this.state.type=='Private'} />
-									Choose Private Membership
-								</label>
+							<div
+								className={cn("card", {selected:(selectedType == 'Private')})}
+								onClick={this.changeType}
+								data-grouptype='Private'
+								>
+								<div id="private" className="card-body">
+									<h4 className='card-title'>Private Membership</h4>
+									<p className='muted small'>Only members can view your group online and must be invited to join.</p>
+									<div className='form-check'>
+										<input className="form-check-input" type="radio" id="privateradio" value='Private' checked={selectedType == 'Private'} />
+										<label className="form-check-label" htmlFor="privateradio">
+											Select private membership
+										</label>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-					<div className="row">
-						<button name="submit" id="submit" type="submit" className="btn btn-primary">Create Group</button>
+					<div className="row mt-4">
+						<div className='col'>
+							<button name="submit" id="submit" type="submit" className="btn btn-secondary">Create Group</button>
+						</div>
 					</div>
 					<input type='hidden' name='session' value={sessionKey} />
 				</form>
-			</div>    
+			</div>
 		);
 	}
 }

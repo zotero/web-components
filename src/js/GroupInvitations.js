ad420c9c859c5e0c0f061c5ec8d0e5cf987cf6f3
Faolan C-P <fcheslack@gmail.com>
2018-07-02 21:57:23 -0400

    update group invitations component

--- src/js/GroupInvitations.js
+++ src/js/GroupInvitations.js
@@ -5,42 +5,22 @@ let log = logger.Logger('GroupInvitations');
 
 import {ajax, postFormData} from './ajax.js';
 import {getCurrentUser} from './Utils.js';
+import {Button, Card, CardHeader, CardBody, Row, Col} from 'reactstrap';
 
 const currentUser = getCurrentUser();
 
-let React = require('react');
+import {Component, Fragment} from 'react';
 
 import {buildUrl} from './wwwroutes.js';
 
-let phpgroupToGroup = function(phpgroup){
-	log.debug('phpgroupToGroup');
-	let group = {
-		id: phpgroup.id,
-		data: {
-			id: phpgroup.id,
-			name: phpgroup.name,
-			type: phpgroup.type,
-			owner: phpgroup.owner,
-			description: phpgroup.description,
-			members: phpgroup.memberIDs,
-			admins: phpgroup.adminIDs,
-			libraryReading: phpgroup.libraryReading,
-			libraryEditing: phpgroup.libraryEditing
-		}
-	};
-	return group;
-};
-
-class GroupInvitation extends React.Component{
+class GroupInvitation extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			done:false
 		};
-		this.acceptInvitation = this.acceptInvitation.bind(this);
-		this.declineInvitation = this.declineInvitation.bind(this);
 	}
-	acceptInvitation(){
+	acceptInvitation = () => {
 		let group = this.props.group;
 		let invitation = this.props.invitation;
 		let url = buildUrl('groupJoin', {group});
@@ -48,7 +28,7 @@ class GroupInvitation extends React.Component{
 			this.setState({done:true});
 		});
 	}
-	declineInvitation(){
+	declineInvitation = () => {
 		let group = this.props.group;
 		let invitation = this.props.invitation;
 		let url = buildUrl('groupDecline', {group, token:invitation.token});
@@ -57,71 +37,79 @@ class GroupInvitation extends React.Component{
 		});
 	}
 	render(){
-		if(this.state.done){
+		const {done} = this.state;
+		if(done){
 			return null;
 		}
-		let group = this.props.group;
+		const {group} = this.props;
 
 		return (
-			<li><strong className="group-title"><a href={buildUrl('groupView', {group})}>{group.data.name}</a></strong> 
+			<Fragment>
+				<strong className="group-title"><a href={buildUrl('groupView', {group})}>{group.data.name}</a></strong> 
 				{/*<span className="group-description">{group.data.description}</span>*/}
 				<div className="group-buttons">
-					<button type="button" onClick={this.acceptInvitation}>Join</button>
-					<button type="button" onClick={this.declineInvitation}>Ignore</button>
+					<Button outline onClick={this.acceptInvitation} className='accept-invitation'>Join</Button>{' '}
+					<Button outline onClick={this.declineInvitation} className='ignore-invitation'>Ignore</Button>
 				</div>
-			</li>
+			</Fragment>
 		);
 	}
 }
 
-class GroupInvitations extends React.Component{
+class GroupInvitations extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			invitations:[],
-			invitationGroups:[]
+			invitationGroups:[],
+			loaded:false
 		};
-		this.loadInvitations = this.loadInvitations.bind(this);
 	}
 	componentDidMount(){
 		this.loadInvitations();
 	}
-	loadInvitations() {
+	loadInvitations = () => {
 		if(currentUser){
 			ajax({url:'/groups/invitations'}).then((resp)=>{
 				resp.json().then((data) => {
 					let invitationGroups = {};
 					data.invitations.forEach((val) => {
-						invitationGroups[val.groupID] = phpgroupToGroup(data.invitationGroups[val.groupID]);
+						invitationGroups[val.groupID] = data.invitationGroups[val.groupID].apiObj;
 					});
 					this.setState({
 						invitations:data.invitations,
-						invitationGroups:invitationGroups
+						invitationGroups:invitationGroups,
+						loaded:true
 					});
 				});
 			});
 		}
 	}
 	render() {
-		let invitationGroups = this.state.invitationGroups;
-		let invitations = this.state.invitations;
+		let {invitationGroups, invitations, loaded} = this.state;
+		if(!loaded){return null;}
+
 		let invitationNodes = invitations.map((invitation)=>{
 			let group = invitationGroups[invitation.groupID];
-			return <GroupInvitation key={invitation.groupID} group={group} invitation={invitation} />;
+			return (
+				<Row key={invitation.groupID}>
+					<Col>
+						<GroupInvitation key={invitation.groupID} group={group} invitation={invitation} />
+					</Col>
+				</Row>
+			);
 		});
 
 		if(invitations.length == 0) {
-			return null;
+			return <span className='group-invitations d-none'></span>;
 		}
 		return (
-			<div className='group-invitations card'>
-				<div className='card-header'>Group Invitations</div>
-				<div className='card-body'>
-					<ul className='list-group list-group-flush'>
-						{invitationNodes}
-					</ul>
-				</div>
-			</div>
+			<Card className='group-invitations'>
+				<CardHeader>Group Invitations</CardHeader>
+				<CardBody>
+					{invitationNodes}
+				</CardBody>
+			</Card>
 		);
 	}
 }

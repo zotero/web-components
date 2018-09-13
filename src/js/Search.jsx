7f3cd75db25a62e4c060d86dc0d4b16ea662b203
Faolan C-P <fcheslack@gmail.com>
2018-09-13 15:27:44 -0400

    Search fixes
    pagination and using location

--- src/js/Search.jsx
+++ src/js/Search.jsx
@@ -6,73 +6,90 @@ let log = logger.Logger('Search.jsx');
 const React = require('react');
 const {Component} = React;
 
-import {Row, Col, Form, Input, FormGroup, InputGroup, Button, Nav, NavItem, NavLink, InputGroupAddon} from 'reactstrap';
+import {Row, Col, Form, Input, FormGroup, InputGroup, Button, Nav, NavItem, NavLink, InputGroupAddon, Pagination, PaginationItem, PaginationLink} from 'reactstrap';
 import {postFormData} from './ajax.js';
 import {LargeUser} from './components/UserList.jsx';
 import {GroupNugget} from './UserGroups.js';
 import {LocationState} from './LocationState.js';
 
-class Pagination extends Component{
+class SearchPagination extends Component{
 	render(){
-		let {basePath, totalResults, page, resultsPerPage} = this.props;
-		let numPages = Math.ceil(totalResults / resultsPerPage);
-		let lc = new LocationState(basePath);
-		let links = [];
+		let {locationState, totalResults, page, resultsPerPage, pageVar} = this.props;
+		let numPages = Math.min(Math.ceil(totalResults / resultsPerPage), 10);
+		let pageLinks = [];
 		for(let i=1; i<numPages+1; i++){
-			lc.setPathVar(pageVar, i);
-			let url = ls.buildUrl(lc.vars.path);
+			locationState.setPathVar(pageVar, i);
+			let url = locationState.buildUrl(locationState.vars.path);
 
-			links.push(<a href={url}>i</a>);
+			pageLinks.push(<PaginationItem active={i == page}>
+				<PaginationLink href={url}>{i}</PaginationLink>
+			</PaginationItem>);
 		}
 		return (
-			<div className='pagination'>
-				{links}
-			</div>
+			<Pagination>
+				{pageLinks}
+			</Pagination>
 		);
 	}
 }
-Pagination.defaultProps = {
+SearchPagination.defaultProps = {
 	pageVar:'p',
 	pageType:'path',
 	page:1,
-	resultsPerPage:10
+	resultsPerPage:10,
+	basePath:'/'
 };
+
 class Search extends Component{
 	constructor(props){
 		super(props);
+		this.ls = new LocationState('/search');
 		this.state = {
 			type:'people',
 			query:'',
 			totalResults:null,
-			results:[]
+			results:[],
+			page:null
 		}
-		this.lc = new LocationState('/search');
+		this.ls.parseVars();
+		if(this.ls.getVar('type')){
+			this.state.type = this.ls.getVar('type');
+		}
+		if(this.ls.getVar('p')){
+			this.state.page = parseInt(this.ls.getVar('p'), 10);
+		}
+		if(this.ls.getVar('q')){
+			this.state.query = this.ls.getVar('q');
+			this.search();
+		}
 	}
 	changeType = (evt) => {
 		evt.preventDefault();
 		let type = evt.target.getAttribute('data-type');
 		this.setState({type, results:[], totalResults:null});
-		this.lc.setPathVar('type', type);
-		this.lc.pushState();
+		this.ls.setPathVar('type', type);
+		this.ls.pushState();
 	}
 	handleQueryChange = (evt) => {
 		this.setState({query:evt.target.value});
 	}
 	search = async(evt) => {
-		evt.preventDefault();
-		const {query, type} = this.state;
+		if(evt){
+			evt.preventDefault();
+		}
+		const {query, type, page} = this.state;
 
 		//change url
-		this.lc.setPathVar('q', query);
-		this.lc.pushState();
+		this.ls.setPathVar('q', query);
+		this.ls.pushState();
 		
 		//perform query
-		let resp = await postFormData('/searchresults', {type, query});
+		let resp = await postFormData('/searchresults', {type, query, page});
 		let results = await resp.json();
 		this.setState({totalResults:results.total, results:results.results});
 	}
 	render() {
-		const {type, results, totalResults} = this.state;
+		const {type, results, query, totalResults, page} = this.state;
 		let resultNodes = null;
 		if(type == 'people'){
 			resultNodes = results.map((user)=>{
@@ -86,7 +103,7 @@ class Search extends Component{
 
 		let pagination = null;
 		if(totalResults > 10) {
-			pagination = <Pagination totalResults={totalResults} />
+			pagination = <SearchPagination locationState={this.ls} totalResults={totalResults} page={page} />
 		}
 		
 		return (
@@ -102,7 +119,7 @@ class Search extends Component{
 				
 				<Form inline onSubmit={this.search} className='my-3'>
 					<InputGroup>
-						<Input type='text' name='query' onChange={this.handleQueryChange} />
+						<Input type='text' name='query' onChange={this.handleQueryChange} defaultValue={query} />
 						<InputGroupAddon addonType="append"><Button onClick={this.search}>Search</Button></InputGroupAddon>
 					</InputGroup>
 				</Form>

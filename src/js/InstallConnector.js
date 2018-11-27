f7684e6e321d2c935640f5b3861cb6bc5a538474
Dan Stillman <dstillman@zotero.org>
2018-11-27 14:06:30 -0700

    Fix download link for old Safari versions
    Serve Safari 9 and earlier 4.0.28-1 instead of redirecting to the Safari
    Extensions Gallery. (Really we should just remove the download link,
    since these browsers aren't safe to use.)

--- src/js/InstallConnector.js
+++ src/js/InstallConnector.js
@@ -19,7 +19,9 @@ const installData = config.installData;
 const {firefoxHash, firefoxVersion} = installData;
 const chromeDownload = 'https://chrome.google.com/webstore/detail/ekhagklcjbdpajgpjgmbionohlpdbjgc';
 const firefoxDownload = `https://www.zotero.org/download/connector/dl?browser=firefox&version=${firefoxVersion}`;
-const safariDownload = 'https://safari-extensions.apple.com/details/?id=org.zotero.zoteroconnectorforsafari-8LAYR367YV';
+const safariDownload = installData.oldSafari
+	? 'https://www.zotero.org/download/connector/dl?browser=safari'
+	: 'https://safari-extensions.apple.com/details/?id=org.zotero.zoteroconnectorforsafari-8LAYR367YV';
 
 class InstallFirefoxButton extends Component{
 	installFirefox(evt){

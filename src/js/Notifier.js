3f81655678d8296f7daf9ee6fb692759ebbb6866
Faolan C-P <fcheslack@gmail.com>
2019-09-03 14:22:40 -0400

    add optional timed redirect to notifier

--- src/js/Notifier.js
+++ src/js/Notifier.js
@@ -1,20 +1,58 @@
 'use strict';
 
-//import {log as logger} from './Log.js';
-//var log = logger.Logger('Notifier');
+import {log as logger} from './Log.js';
+var log = logger.Logger('Notifier');
 
-const React = require('react');
+import {useState, useEffect, useRef} from 'react';
 
-class Notifier extends React.Component {
-	render() {
-		if(!this.props.message){
-			return null;
+let Notifier = function(props) {
+	const {message, type, redirect, redirectLabel} = props;
+	const [startTime, setStartTime] = useState(Date.now());
+	const [timeLeft, setTimeLeft] = useState(5);
+	const intervalRef = useRef();
+
+	//periodically check how much time is left before we should redirect, then redirect
+	const intervalCallback = () => {
+		let nowMS = Date.now();
+		let elapsedMS = nowMS - startTime;
+		log.debug(`elapsedMS: ${elapsedMS}`);
+		let secsLeft = Math.max(0, Math.floor((5500 - elapsedMS) / 1000));
+		log.debug(`secsLeft: ${secsLeft}`);
+		setTimeLeft(secsLeft);
+		
+		if(elapsedMS >= 5000) {
+			clearInterval(intervalRef.current);
+			window.location.href = redirect;
 		}
-		let className = `notifier ${this.props.type}`;
-		return (
-			<div className={className} role='alert'>{this.props.message}</div>
-		);
+	};
+
+	useEffect(() => {
+		//start timer interval to count down if we have a redirect
+		if (redirect) {
+			intervalRef.current = setInterval(intervalCallback, 500);
+		}
+	}, []);
+
+	if(!message){
+		return null;
 	}
-}
+	let timerMessage = null;
+	
+	if (redirect) {
+		if (timeLeft <= 0) {
+			window.location.href = redirect;
+		}
+		if (redirectLabel) {
+			timerMessage = <p>Redirecting to {redirectLabel} in {timeLeft}...</p>;
+		} else {
+			timerMessage = <p>Leaving in {timeLeft}...</p>;
+		}
+	}
+
+	let className = `notifier ${type}`;
+	return (
+		<div className={className} role='alert'>{message}{timerMessage}</div>
+	);
+};
 
 export {Notifier};

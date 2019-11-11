a764f3939176b795ffa1617235215ef4dbd28f4e
Faolan C-P <fcheslack@gmail.com>
2019-11-11 15:18:11 -0500

    de-lint and make function component

--- src/js/profile/message-user-button.jsx
+++ src/js/profile/message-user-button.jsx
@@ -1,28 +1,22 @@
-'use strict';
-
 // import {log as logger} from '../Log.js';
 // let log = logger.Logger('MessageUserButton');
 
-const React = require('react');
-const {Component} = React;
-import {PropTypes} from 'prop-types';
-import {getCurrentUser} from '../Utils.js';
+import { PropTypes } from 'prop-types';
+import { getCurrentUser } from '../Utils.js';
 
-class MessageUserButton extends Component {
-	render(){
-		const {username} = this.props;
-		const curUser = getCurrentUser();
-		if(!curUser){
-			return null;
-		}
-		if(curUser.username == username){
-			return null;
-		}
-		return (<a id='message-user' className='btn btn-secondary' href={`//forums.zotero.org/messages/add/${username}`} >Send Message</a>);
+function MessageUserButton(props) {
+	const { username } = props;
+	const curUser = getCurrentUser();
+	if (!curUser) {
+		return null;
 	}
+	if (curUser.username == username) {
+		return null;
+	}
+	return (<a id='message-user' className='btn btn-secondary' href={`//forums.zotero.org/messages/add/${username}`} >Send Message</a>);
 }
 MessageUserButton.propTypes = {
 	username: PropTypes.string.isRequired
 };
 
-export {MessageUserButton};
+export { MessageUserButton };

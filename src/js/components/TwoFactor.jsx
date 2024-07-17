7ab21bcdce3b3313366244f9a6864661605f46bb
Faolan C-P <fcheslack@gmail.com>
2024-07-17 15:18:23 -0400

    update ManageTwoFactor

--- src/js/components/TwoFactor.jsx
+++ src/js/components/TwoFactor.jsx
@@ -6,24 +6,64 @@ let log = logger.Logger('TwoFactor');
 
 import { Notifier } from '../Notifier.js';
 import { postFormData } from '../ajax.js';
+import { Badge, Button, Card, CardBody, Input } from 'reactstrap';
 
 const registerFidoUrl = '/settings/setverificationpreference';
 const fidoUrl = '/user/verifymfa';
 const setVerificationPrefUrl = '/settings/setverificationpreference';
 
 function SavedSecurityKey(props) {
-	const removeKey = () => {
-		//TODO
+	const removeKey = async () => {
+		log.debug("removeKey");
+		log.debug(props);
+		if (confirm('Are you sure you want to delete this security key from your account? This may make your account less secure.')) {
+			let resp = await postFormData(
+				registerFidoUrl,
+				{
+					verificationMethod: 'u2f',
+					u2fAction: 'deleteKey',
+					u2fNickname: props.nickname,
+					u2fKeyID: props.keyID,
+				},
+				{method: 'POST', withSession:true}
+			);
+			let data = await resp.json();
+			if(!resp.ok || data.success == false) {
+				setNotification({type:'error', message:data.error});
+				return;
+			}
+			window.location.reload();
+		}
 	};
 
+	let d = new Date(props.dateAdded);
+
 	return (
-		<div className='security-key striped clearfix'>
+		<div className='security-key'>
 			<span className='security-key-nickname'>{props.nickname}</span>
-			<span className='security-key-note'>— registered on {props.dateAdded}</span>
-			<button className='btn btn-secondary float-right' onClick={removeKey}>Remove Key</button>
+			<span className='security-key-note'>— registered on {d.toLocaleString()}</span>
+			<Button className='btn btn-secondary' onClick={removeKey}>Remove Key</Button>
 		</div>
 	);
 };
+
+function EnabledTag(props) {
+	let classes = 'security-enabled-tag';
+	let text = '';
+	let color;
+	if (props.pending) {
+		text = 'Pending';
+		classes += ' pending';
+		color = 'warning';
+	} else if(props.enabled) {
+		text = 'Enabled';
+		classes += ' enabled';
+		color = 'success';
+	}
+	return (
+		<Badge color={color} className={classes}>{text}</Badge>
+	)
+}
 
 function VerifierForm(props) {
 	const [verifierCode, setVerifierCode] = useState('');
@@ -51,24 +91,25 @@ function VerifierForm(props) {
 				return;
 			}
 
-			props.setPrefResponseData(data);
+			props.setPrefResponseData(Object.assign({}, props.prefResponseData, data));
 
 			switch (props.verificationMethod) {
 				case 'totp':
+					props.removeVerificationRequirement('totp');
 					if (data.message) {
 						props.setNotification({type: 'success', message: data.message});
-
-						if (data.enabled === false) {
-							props.setTOTPVerificationEnabled(false);
-						}
+					}
+					if (data.enabled === false) {
+						props.setTOTPVerificationEnabled(false);
 					}
 					break;
 				case 'email':
+					props.removeVerificationRequirement('email');
 					if (data.message) {
 						props.setNotification({type: 'success', message: data.message});
-						if (data.enabled === false) {
-							props.setEmailVerificationEnabled(false);
-						}
+					}
+					if (data.enabled === false) {
+						props.setEmailVerificationEnabled(false);
 					}
 			}
 		} catch(err) {
@@ -82,26 +123,28 @@ function VerifierForm(props) {
 	const rememberEl = props.remember ? (
 		<li>
 			<label for="remember_device">Remember Device
-				<input type="hidden" name="remember_device" value="0" />
-				<input type="checkbox" name="remember_device" id="remember_device" value="1" className="checkbox" onChange={(evt)=>{setRememberDevice(evt.target.value);}} />
+				<Input type="hidden" name="remember_device" value="0" />
+				<Input type="checkbox" name="remember_device" id="remember_device" value="1" className="checkbox" onChange={(evt)=>{setRememberDevice(evt.target.value);}} />
 			</label>
 			<p className="hint">Don't require two-step verification on this device in the future.</p>
 		</li>
 	) : null;
 
 	return (
-		<form className='verifier-form zform' encType="application/x-www-form-urlencoded" acceptCharset="utf-8" method="post" onSubmit={submitFunc}>
-			<ol>
-				<li><label htmlFor="verifier_code">Multifactor Code <input type="text" name="verifier_code" id="verifier_code" maxLength="6" autoFocus="1" value={verifierCode} onChange={(evt)=>{setVerifierCode(evt.target.value);}} /></label>
-					<p className="hint">Enter the six digit code from your email or authenticator app</p>
-				</li>
-				{rememberEl}
-				<li>
-					<button className='btn btn-secondary' name="verify_multifactor_submit" id="verify_multifactor_submit" type="submit" value="verify_multifactor_submit">Verify</button>
-					<button className='btn btn-secondary' name="verify_multifactor_cancel" id="verify_multifactor_cancel" type="submit" value="1" onClick={props.cancel}>Cancel</button>
-				</li>
-			</ol>
-		</form>
+		<div className='verifier-form-container'>
+			<form className='verifier-form zform' encType="application/x-www-form-urlencoded" acceptCharset="utf-8" method="post" onSubmit={submitFunc}>
+				<ol>
+					<li><label htmlFor="verifier_code">Multifactor Code <Input type="text" name="verifier_code" id="verifier_code" maxLength="6" autoFocus="1" value={verifierCode} onChange={(evt)=>{setVerifierCode(evt.target.value);}} /></label>
+						<p className="hint">Enter the six digit code from your email or authenticator app</p>
+					</li>
+					{rememberEl}
+					<li>
+						<Button className='btn btn-secondary' name="verify_multifactor_submit" id="verify_multifactor_submit" type="submit" value="verify_multifactor_submit">Verify</Button>
+						<Button className='btn btn-secondary' name="verify_multifactor_cancel" id="verify_multifactor_cancel" type="submit" value="1" onClick={props.cancel}>Cancel</Button>
+					</li>
+				</ol>
+			</form>
+		</div>
 	);
 }
 
@@ -139,7 +182,7 @@ function RegisterFidoKey(props) {
 				log.error(error);
 				let message;
 				if (error.name === 'InvalidStateError') {
-					message = 'Error: Authenticator was probably already registered by user';
+					message = 'Registration failed. Authenticator may have already been registered for this user.';
 				} else {
 					message = error.name;
 				}
@@ -158,33 +201,38 @@ function RegisterFidoKey(props) {
 				{method:'POST', withSession:true}
 			);
 			const verificationJSON = await verificationResp.json();
-			log.debug(verificationJSON);
 			// prompt server response
 			if (verificationJSON.success) {
-				// reloadServerPreview();
-				props.setNotification({type:'success', message:'Authenticator Registered'});
+				props.setNotification({type:'success', message:'Security Key Registered'});
+				window.location.reload();
 			} else {
-				props.setNotification({type:'error', message:'Authenticator Registration Failed'});
+				let message = verificationJSON.message ?? 'Security Key Registration Failed';
+				props.setNotification({type:'error', message});
 				return;
 			}
 		} catch (err) {
-			// reloadServerPreview();
 			log.error(err);
-			props.setNotification({type:'error', message:'Authenticator Registration Failed'});
+			if (err instanceof Response) {
+				let data = await err.json();
+				let message = data.message ?? 'Security Key Registration Failed';
+				props.setNotification({type:'error', message});
+			} else {
+				props.setNotification({type:'error', message:'Security Key Registration Failed'});
+			}
 			return;
 		}
 	}
 	
 
 	return (
-		<div id='new-security-key' className='hidden'>
+		<div id='new-security-key'>
 			<form onSubmit={createFidoRegistration}>
-				<input 
+				<Input 
 					type='text'
 					name='nickname'
 					placeholder='Security Key Nickname'
 					value={newKeyNickname} onChange={(evt) => { setNewKeyNickname(evt.target.value); }} />
-				<button id='create-security-key-button' disabled={(newKeyNickname.length == 0)}>Add Key</button>
+				<Button id='create-security-key-button' disabled={(newKeyNickname.length == 0)}>Add Key</Button>
 			</form>
 		</div>
 	);
@@ -198,29 +246,32 @@ function ManageTwoFactor(props) {
 	const [fidoKeys, setFidoKeys] = useState(props.securityKeysSummary);
 	const [prefResponseData, setPrefResponseData] = useState(props);
 	const [newKey, setNewKey] = useState(false);
+	const [editKeys, setEditKeys] = useState(false);
+	const [verificationTestRequired, setVerificationTestRequired] = useState(props.verificationTestRequired);
 	
-	/*
-	const [qrDataUrl, setQrDataUrl] = useState(props.qrDataUrl);
-	const [secretText, setSecretText] = useState(props.secretText);
-	const [newTOTPRequired, setNewTOTPRequired] = useState(props.newTOTPRequired);
-	const [TOTPVerificationTestRequired, setTOTPVerificationTestRequired] = useState(props.TOTPVerificationTestRequired);
-	const [message, setMessage] = useState(props.message);
-
-	this.qrDataUrl = data.qrDataUrl ?? this.qrDataUrl;
-	this.secretText = data.secretText ?? this.secretText;
-	this.newTOTPRequired = data.newTOTPRequired ?? this.newTOTPRequired;
-	this.TOTPVerificationTestRequired = data.TOTPVerificationTestRequired ?? this.TOTPVerificationTestRequired;
-	this.message = data.message ?? this.message;
-	*/
-	const toggleEmail = (evt) => {
-		let enable = evt.target.checked;
-		setVerificationPreference('totp', enable);
+	const removeVerificationRequirement = (method) => {
+		setVerificationTestRequired(verificationTestRequired.filter((val) => val != method));
 	};
 
-	const toggleTOTP = (evt) => {
-		let enable = evt.target.checked;
-		setVerificationPreference('totp', enable);
+	const enableEmail = async () => {
+		setVerificationPreference('email', true);
 	};
+	const disableEmail = async () => {
+		if (confirm('Are you sure you want to disable two-step verification using email? This may make your account less secure.')) {
+			setVerificationPreference('email', false);
+			// removeVerificationRequirement('email');
+		}
+	}
+
+	const enableTOTP = async () => {
+		setVerificationPreference('totp', true);
+	};
+	const disableTOTP = async () => {
+		if (confirm('Are you sure you want to disable two-step verification using an authenticator app? This may make your account less secure.')) {
+			setVerificationPreference('totp', false);
+			// removeVerificationRequirement('totp');
+		}
+	}
 
 	const setVerificationPreference = async (verificationMethod, enabledVal) => {
 		// console.log(`fetching ${config.uploadUrl}`);
@@ -232,14 +283,24 @@ function ManageTwoFactor(props) {
 				setNotification({type:'error', message:data.error});
 				return;
 			}
-			setPrefResponseData(data);
+			setPrefResponseData(Object.assign({}, prefResponseData, data));
 			setNotification({type:'success', message:data.message});
 			switch (verificationMethod) {
 				case 'email':
 					setEmailVerificationEnabled(enabled);
+					if (enabled) {
+						setVerificationTestRequired(verificationTestRequired.concat('email'))
+					} else {
+						removeVerificationRequirement('email');
+					}
 					break;
 				case 'totp':
 					setTOTPVerificationEnabled(enabled);
+					if (enabled) {
+						setVerificationTestRequired(verificationTestRequired.concat('totp'))
+					} else {
+						removeVerificationRequirement('totp');
+					}
 					break;
 				default:
 					throw "Unexpected verificationMethod";
@@ -251,8 +312,17 @@ function ManageTwoFactor(props) {
 		}
 	};
 
+	let verifierProps = {
+		setNotification,
+		prefResponseData,
+		setPrefResponseData,
+		setEmailVerificationEnabled,
+		setTOTPVerificationEnabled,
+		removeVerificationRequirement,
+	};
+
 	let activateTOTPEl = null;
-	if (prefResponseData && prefResponseData['qrDataUrl']) {
+	if (verificationTestRequired.includes('totp') && prefResponseData && prefResponseData['qrDataUrl']) {
 		activateTOTPEl = (
 			<div id='activate-totp' className='activation-section'>
 				<div id='secret-code'>
@@ -261,20 +331,24 @@ function ManageTwoFactor(props) {
 					<img id='qr-code-img' src={prefResponseData['qrDataUrl']} />
 					<br />
 					<p id='secret-text'>{prefResponseData['secretText']}</p>
-					<VerifierForm {...{setNotification, setPrefResponseData, setEmailVerificationEnabled, setTOTPVerificationEnabled, verificationMethod: 'totp'}} />
+					<VerifierForm {...Object.assign({},verifierProps, {verificationMethod:'totp'})} />
 				</div>
 			</div>
 		);
 	}
 
 	let activateEmailEl = null;
-	if (props.verificationTestRequired.includes('email')) {
+	if (verificationTestRequired.includes('email')) {
 		activateEmailEl = (
 			<div id='activate-email' className='activation-section'>
-				<div id='secret-code'>
-					<p className='text-center'>Email authentication chosen. We've sent a code to {props.primaryEmail}. Enter it below to complete 2-step verification setup.</p>
-					<VerifierForm {...{setNotification, setPrefResponseData, setEmailVerificationEnabled, setTOTPVerificationEnabled, verificationMethod: 'email'}} />
-				</div>
+				<Card>
+					<CardBody>
+						<div id='secret-code'>
+							<p className='text-center'>Email authentication chosen. We've sent a code to {props.primaryEmail}. Enter it below to complete 2-step verification setup.</p>
+							<VerifierForm {...Object.assign({},verifierProps, {verificationMethod:'email'})} />
+						</div>
+					</CardBody>
+				</Card>
 			</div>
 		);
 	}
@@ -284,42 +358,52 @@ function ManageTwoFactor(props) {
 	});
 
 	return (
-		<div>
+		<div className='manage-two-factor-container'>
 			<Notifier {...notification} />
 			<div id='verifications'>
 				<div className='verification-method-section' id='email'>
-					<h3>Email Authentication</h3>
-					<p>You will receive an authentication code emailed to: <span id='user-email'></span></p>
-					<label>
-						<input type="checkbox" className='toggle-check' id='email-verification-check' onChange={toggleEmail} checked={emailVerificationEnabled|props.verificationTestRequired.includes('email')} />
-						Email Verification
-					</label>
+					<h4>Email Authentication</h4><EnabledTag enabled={emailVerificationEnabled} pending={verificationTestRequired.includes('email')} />
+					<p>Receive an authentication code emailed to: <span id='user-email'>{props.primaryEmail}</span></p>
+					{emailVerificationEnabled|verificationTestRequired.includes('email') ?
+							<Button className='btn btn-secondary' id='disable-email-verification-button' onClick={disableEmail}>Disable</Button> :
+							<Button className='btn btn-secondary' id='enable-email-button' onClick={enableEmail}>Enable</Button>
+						}
 					{activateEmailEl}
 				</div>
 				<div className='verification-method-section' id='totp'>
-					<h3>Authenticator App</h3>
+					<h4>Authenticator App</h4><EnabledTag enabled={TOTPVerificationEnabled} pending={verificationTestRequired.includes('totp')} />
 					<p>Use an authenticator app or browser extension to get two-factor authentication codes when prompted.</p>
-					<label>
-						<input type="checkbox" className='toggle-check' id='totp-verification-check' onChange={toggleTOTP} checked={TOTPVerificationEnabled|props.verificationTestRequired.includes('totp')} />
-						Authenticator App
-					</label>
+					{TOTPVerificationEnabled|verificationTestRequired.includes('totp') ?
+						<Button className='btn btn-secondary' id='disable-totp-button' onClick={disableTOTP}>Disable</Button> :
+						<Button className='btn btn-secondary' id='enable-totp-button' onClick={enableTOTP}>Enable</Button>
+					}
 					{activateTOTPEl}
 				</div>
-				<div className='verification-method-section' id='totp'>
-					<h3>Security Key</h3>
+				<div className='verification-method-section' id='fido'>
+					<h4>Security Key</h4><EnabledTag enabled={savedKeyEls.length > 0} />
+					{editKeys ? 
+						<Button className='btn btn-secondary float-right' onClick={()=>{setEditKeys(false);}}>Hide</Button>
+						:<Button className='btn btn-secondary float-right' onClick={()=>{setEditKeys(true);}}>Edit</Button>
+					}
 					<p>Use a hardware device that can act as your second factor of authentication.</p>
-					<div id='saved-security-keys' className='striped'>
+					{editKeys ?
+						<div id='edit-security-keys'>
+							<div id='saved-security-keys'>
 								{savedKeyEls}
-					</div>
-					<div>
-						<button className='btn btn-secondary' id='show-add-security-key-button' onClick={()=>{setNewKey(true);}}>Register a new security key</button>
-						{newKey ? 
-							<RegisterFidoKey {...{setNotification}} />
-							: null
-						}
-					</div>
-					<div id='activate-security-key' className='activation-section hidden'>
-					</div>
+							</div>
+							<div className='security-key'>
+								<Button className='btn btn-secondary' id='show-add-security-key-button' onClick={()=>{setNewKey(true);}}>Register a new security key</Button>
+								{newKey ? 
+									<RegisterFidoKey {...{setNotification}} />
+									: null
+								}
+							</div>
+						</div>
+						:null
+					}
+				</div>
+				<div className='recovery-section'>
+
 				</div>
 			</div>
 		</div>
@@ -331,17 +415,12 @@ ManageTwoFactor.defaultProps = {
 	fidoKeys: [],
 }
 
-function AppVerifier(props) {
-
-}
-
 function U2FVerifier(props) {
 	const [notification, setNotification] = useState(null);
-	const [remember, setRemember] = useState(false);
 	
 	const verify = async () => {
 		try {
-			const result = await verifyU2F(remember);
+			const result = await verifyU2F();
 			if (result.success) {
 				setNotification({type: 'success', message: 'Authenticator Verified'});
 				if (props.redirect) {
@@ -365,15 +444,8 @@ function U2FVerifier(props) {
 				When you are ready, authenticate using the button below.
 			</p>
 			<p>
-				<button className='btn btn-secondary' id='begin-u2f-button' onClick={verify}>Use Security Key</button>
+				<Button className='btn btn-secondary' id='begin-u2f-button' onClick={verify}>Use Security Key</Button>
 			</p>
-			<div className='zform'>
-				<div className="form-group">
-					<input type="checkbox" name="remember_device" id="remember_device" value="1" class="checkbox" onChange={(evt) => {setRemember(evt.target.value)}} />
-					&nbsp;<label for="remember_device" class="optional">Remember Device</label>
-					<p class="hint text-muted small">Don't require two-step verification on this device in the future.</p>
-				</div>
-			</div>
 		</div>
 	);
 };
@@ -381,7 +453,7 @@ U2FVerifier.defaultProps = {
 	redirect: true,
 };
 
-async function verifyU2F(remember = false) {
+async function verifyU2F() {
 	log.debug('verifyU2F');
 
 	try {
@@ -391,7 +463,7 @@ async function verifyU2F(remember = false) {
 		
 		// get auth check args
 		log.debug('getting authArgs');
-		let resp = await postFormData(fidoUrl, {method: 'u2f', u2fAction: 'startAuth'}, {type:'POST', withSession:true})
+		let resp = await postFormData(fidoUrl, {u2fAction: 'startAuth'}, {type:'POST', withSession:true})
 		
 		let asseResp;
 		try {
@@ -403,11 +475,8 @@ async function verifyU2F(remember = false) {
 		}
 
 		// log.debug(asseResp);
-		let processAuthData = {method: 'u2f', u2fAction: 'processAuth', u2fPayload: JSON.stringify(asseResp)};
-		if (remember) {
-			processAuthData.remember_device = '1';
-		}
-		resp = await postFormData(fidoUrl, processAuthData, {type:'POST', withSession:true});
+		
+		resp = await postFormData(fidoUrl, {u2fAction: 'processAuth', u2fPayload: JSON.stringify(asseResp)}, {type:'POST', withSession:true});
 		// log.debug(resp);
 		const authServerResponse = await resp.json();
 		// log.debug(authServerResponse);
